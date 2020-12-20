20abbeb680d7f55c6da11a67b4f731dcdb43cb37
PhilGarb <phil.garbowsky@philtech.dev>
2020-12-20 11:21:20 +0100

    fix: :wrench: changed Backend url to new heroku deploy

--- src/features/Data/DataFetchingClient.ts
+++ src/features/Data/DataFetchingClient.ts
@@ -2,7 +2,7 @@ import { createClient, defaultExchanges } from "urql";
 import { devtoolsExchange } from "@urql/devtools";
 
 export const client = createClient({
-  url: "https://builder.open-decision.org/graphql",
+  url: "https://od-backend-test.herokuapp.com/graphql",
   exchanges: [devtoolsExchange, ...defaultExchanges],
   fetchOptions: {
     credentials: "include",
