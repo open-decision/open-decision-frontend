0972f2b8cac3b69802ecc91344035d5a742f0c22
PhilGarb <phil.garbowsky@philtech.dev>
2021-02-11 13:05:35 +0100

    refactor: :art: Improved quality of calculateCurve

--- src/features/Builder/node-editor/utilities/shared.ts
+++ src/features/Builder/node-editor/utilities/shared.ts
@@ -1,16 +1,30 @@
 import { curveBasis, line } from "d3-shape";
 import { connectionCoordinates } from "../types";
 
+const minimumDistanceFromPort = 15;
+
+/**
+ * Calculates the path for the svg connection between Ports.
+ * @param connectionCoordinates - The coordinates of the start port and end port.
+ */
 export const calculateCurve = (
   connectionCoordinates: connectionCoordinates
 ): string | null => {
   const [origin, destination] = connectionCoordinates;
+  /**
+   * The distance between the Nodes on the x axis.
+   */
   const length = origin[0] - destination[0];
 
+  //The passed in array defines the coordinates of the curve.
   return line().curve(curveBasis)([
-    [origin[0], origin[1]],
-    [origin[0] - Math.max(length / 3, 15), origin[1]],
-    [destination[0] + Math.max(length / 3, 15), destination[1]],
-    [destination[0], destination[1]],
+    origin,
+    //The next two coordinates make sure that a minimun distance from the port is maintained. This makes the curve look better.
+    [origin[0] - Math.max(length / 3, minimumDistanceFromPort), origin[1]],
+    [
+      destination[0] + Math.max(length / 3, minimumDistanceFromPort),
+      destination[1],
+    ],
+    destination,
   ]);
 };
