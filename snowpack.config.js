5d5270176e7e6668adf81ece45cb95c8f038f2a5
PhilGarb <phil.garbowsky@philtech.dev>
2021-03-03 14:35:05 +0100

    build: :hammer: Switched to snowpack esbuild

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,10 +4,14 @@ module.exports = {
   mount: { public: { url: "/", static: true }, src: { url: "/dist" } },
   plugins: [
     "@snowpack/plugin-postcss",
-    "@snowpack/plugin-typescript",
-    "@snowpack/plugin-webpack",
     "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-typescript",
   ],
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: "es2018",
+  },
   devOptions: { open: "none", port: 3000 },
   alias: {
     "@internalTypes": "./src/types",
