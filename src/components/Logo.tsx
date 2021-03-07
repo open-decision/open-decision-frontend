67c11ef992e165c1c9f9170e999398a1dce05f01
PhilGarb <phil.garbowsky@philtech.dev>
2021-03-07 11:24:48 +0100

    refactor: :truck: Moved assets to public because import paths were not adjusted correctly

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,11 @@
 import clsx from "clsx";
 import React from "react";
 import { Link } from "./Links";
-import logo from "../assets/icons/OD_LOGO.svg";
 
 export const Logo: React.FC<Record<string, unknown>> = () => (
   <Link to="/">
     <h1 className={clsx("w-44")}>
-      <img src={logo} alt="open-decision logo" />
+      <img src="/assets/OD_LOGO.svg" alt="open-decision logo" />
     </h1>
   </Link>
 );
